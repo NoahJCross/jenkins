3643582b100f59c5726170390516c3377ddbabdf
NoahJCross <164634613+NoahJCross@users.noreply.github.com>
2024-07-21 01:49:40 +1000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,20 +13,46 @@ pipeline {
                 }
             }
         }
-        stage("Test"){
-          steps{
-            echo "Testing..."
-          }
+        stage('Build') {
+            steps {
+                echo 'Building the code using Maven...'
+            }
         }
-        stage("Deploy"){
-          steps{
-            echo "Deploying..."
-          }
+
+        stage('Unit and Integration Tests') {
+            steps {
+                echo 'Running unit and integration tests using JUnit and Selenium...'
+            }
         }
-        stage("Complete"){
-          steps{
-            echo "Completed..."
-          }
+
+        stage('Code Analysis') {
+            steps {
+                echo 'Performing code analysis using SonarQube...'
+            }
+        }
+
+        stage('Security Scan') {
+            steps {
+                echo 'Running security scan using OWASP ZAP...'
+            }
+        }
+
+        stage('Deploy to Staging') {
+            steps {
+                echo 'Deploying to staging using AWS CodeDeploy...'
+            }
+        }
+
+        stage('Integration Tests on Staging') {
+            steps {
+                echo 'Running integration tests on staging using Postman...'
+            }
+        }
+
+        stage('Deploy to Production') {
+            steps {
+                echo 'Deploying to production using AWS CodeDeploy...'
+            }
         }
     }
 }
