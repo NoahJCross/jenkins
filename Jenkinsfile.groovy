60741ce0d1ef3dd910563300736516a4891df1e1
Noah Cross <s223226235@deakin.edu.au>
2024-09-05 17:47:56 +1000

    updates

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage("Build") {
             steps {
-                echo "Building the project using Maven."
+                echo "Building the project using Maven"
             }
             post {
                 always {
